73ef18a1c4b993723573b8ddbae3ed8c17907719
Christopher Chase <christopher.chase@dice205.com>
2022-12-03 22:16:16 +0800

    Initial Commit and Done

---
+++ zipper.ps1
@@ -0,0 +1,32 @@
+<# Chase #>
+
+Write-Host "Welcome to Zipper`n`n"
+
+$path = Read-Host "`nEnter folder directory"
+
+if (Test-Path -Path $path) {
+    <# Action to perform if the condition is true #>
+
+    $file_name = Read-Host "`nEnter Zip file name"
+    
+    # Execute the Compress-Archive command
+    $compress_files = @{
+        Path = $path
+        CompressionLevel = "Fastest"
+        DestinationPath = $path+"\"+$file_name+".zip"
+    }
+
+    Compress-Archive @compress_files
+
+
+    Write-Host "`nZipped File successfully created and located to: "$path"\"$file_name".zip"
+
+    Start-Process $path
+
+}
+else{
+    Write-Host "`nDirectory not found!!!`n`n"
+}
+
+# Prevent from exit
+Pause
